ffbecbe2dc1b1e637ecdecf32a6bd5486d8d70ca
Kamil <259299@student.pwr.edu.pl>
2021-03-04 18:23:39 +0100

    Dopisanie extern

--- test3/modul.c
+++ test3/modul.c
@@ -3,5 +3,5 @@
  *  Zestaw wazniejszych stalych
  */
 
-extern const double PI = 3.141593; /* Dopisanie 'extern' przed stala*/
-extern const double E  = 2.718282;
+extern const double PI = 3.141593; /* Dopisanie 'extern' przed stala dla C++ */
+extern const double E  = 2.718282; /* Dopisanie 'extern' przed stala dla C++ */
