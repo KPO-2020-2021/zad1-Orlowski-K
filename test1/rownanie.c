5be1a7c857b9b7561892f95aa38c6ffd4487ab6a
Kamil <259299@student.pwr.edu.pl>
2021-03-04 16:23:40 +0100

    Biblioteka matematyczna w C

--- test1/rownanie.c
+++ test1/rownanie.c
@@ -41,7 +41,7 @@ int main() {
   
   if (Delta_2 > 0) {
 
-      Delta = sqrt( Delta_2 );
+      Delta = sqrt( Delta_2 );  /* W kompilatorze C nalezy dodac biblioteke matematyczna '-lm'; W kompilatorze C++ program sie kompiluje bez bledow */
       X1 = (-TKw.b - Delta) / (2*TKw.a);
       X2 = (-TKw.b + Delta) / (2*TKw.a);
       Ilosc_Pierwiastkow = 2;      
